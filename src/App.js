4954b006a0400105df7b569ac51dc71994e4ab8b
shivam datta purkayastha <shivamdatta465@gmail.com>
2023-12-31 20:22:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
-
+import React from 'react';
+import Row from './Row';
+import requests from './requests';
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone </h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchTrending}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+
     </div>
   );
 }
